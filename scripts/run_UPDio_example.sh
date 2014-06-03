8ee196b9350382e469a2fc21bd50fa36110b59a1
Dan King <dk6@sanger.ac.uk>
2014-06-03 23:17:32 +0100

    remove duplicated directories and files

--- scripts/run_UPDio_example.sh
+++
@@ -1,2 +0,0 @@
-# Run from trunk   ( UPDio/Version1/ )
-perl UPDio.pl --child_vcf sample_data/child.exome.sorted.homREFed.vcf.gz --mom_vcf sample_data/mom.exome.sorted.homREFed.vcf.gz --dad_vcf sample_data/dad.exome.sorted.homREFed.vcf.gz
