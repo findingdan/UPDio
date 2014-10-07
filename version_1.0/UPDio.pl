8199e4ff4a8061670f8451d7788e2603f8cc8393
Dan King <dk6@sanger.ac.uk>
2014-10-07 16:05:27 +0100

    updated licence

--- version_1.0/UPDio.pl
+++ version_1.0/UPDio.pl
@@ -850,16 +850,24 @@ for searching for mendelian inconsistent sites; occassionally useful for finding
 
 =head1 LICENSE
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  For more information, please go to <http://www.gnu.org/licenses/>.
+  Copyright (c) 2007 Genome Research Ltd.
+  Author: Joe Bloggs <optional@address>
+  
+  Any redistribution or derivation in whole or in part including any
+  substantial portion of this code must include this copyright and
+  permission notice.
+  
+  THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  
+  This code is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation; either version 2 of the License, or (at
+  your option) any later version (http://www.gnu.org/copyleft/gpl.txt).
 
 =cut
